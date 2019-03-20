c947d9ae9cc9e7c7d2ce9ae1620df86ed1c8abea
Parick Cowan <patrick.w.cowan@gmail.com>
2019-03-20 15:18:32 -0500

    wednesday exercise push

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Where do you live?");
+//        String locationHome = scanner.nextLine();
+//        System.out.format("You live in %s\n",locationHome);
+//        System.out.println("Where do you want to live?");
+//        String locationDream = scanner.nextLine();
+//        System.out.format("You live in %s, but you want to live %s",locationHome,locationDream);
+
+//        double pi = 3.14159;
+//
+//        System.out.println("The value of pi is approximately " + pi);
+//        System.out.format("The value of pi is approximately %.2f", pi);
+
+//        System.out.println("Enter a number 1 - 10.");
+//        int initialNumber = scanner.nextInt();
+//        System.out.println("You entered: " + initialNumber);
+//        //If you type anything other than a integer it will give a mismatch
+
+//        System.out.println("Enter your First Middle and Last name: ");
+//        String firstName = scanner.next();
+//        String middleName = scanner.next();
+//        String lastName = scanner.next();
+//
+//        System.out.format("Youre name is: %s %s %s", firstName,middleName,lastName);
+//        // If you type less than 3 words it sticks and won't continue until you do
+//        // If you type more than 3 words it only takes the first 3
+
+//        System.out.println("What is your favorite movie quote?");
+//        String favQuote = scanner.next();
+//        System.out.format("Your favorite movie quote is: %s", favQuote);
+//        //I don't capture all of the words because I used .next()
+
+//        System.out.println("What is your favorite movie quote?");
+//        String favQuote = scanner.nextLine();
+//        System.out.format("Your favorite movie quote is: %s", favQuote);
+
+        System.out.println("Calculate the perimeter and area of a room!");
+        System.out.println("Enter the Length and Width of the room: ");
+//        double userLength = Double.parseDouble(scanner.next());
+//        double userWidth = Double.parseDouble(scanner.next());
+
+        int userLength = scanner.nextInt();
+        int userWidth = scanner.nextInt();
+
+        double area = userLength * userWidth;
+        double perimeter = (2 * userLength) + (2 * userWidth);
+
+        System.out.format("The area of your room is: %.2f\n",area );
+        System.out.format("The perimeter of your room is: %.2f ",perimeter);
+
+        System.out.println("What is the height of your room?");
+        int userHeight = scanner.nextInt();
+        double volume = userHeight * userLength * userWidth;
+        System.out.println("The volume of the room is: " + volume);
+
+
+
+
+
+
+    }
+}
